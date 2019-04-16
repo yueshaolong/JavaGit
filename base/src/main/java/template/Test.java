087fc0a52cdb48665cf132b985fde0d34ea54b6c
yueshaolong <yueshaolong@vsxin.com>
2019-04-16 10:35:38 +0800

    模板方法模式

---
+++ base/src/main/java/template/Test.java
@@ -0,0 +1,13 @@
+package template;
+
+public class Test {
+    public static void main(String[] args) {
+        Boss boss = new Boss();
+        boss.oneDay();
+
+        System.out.println("--------------------------");
+
+        Worker worker = new Worker();
+        worker.oneDay();
+    }
+}
