f803bfd8ca01714c669ecd9c5e47facf17b40ad2
yueshaolong <yueshaolong@vsxin.com>
2019-04-17 14:19:00 +0800

    责任链模式

---
+++ base/src/main/java/chain/bidirectionalchain/Response.java
@@ -0,0 +1,15 @@
+package chain.bidirectionalchain;
+
+//封装响应信息的类Response
+public class Response {
+    String responseStr;
+
+    public String getResponse() {
+        return responseStr;
+    }
+
+    public void setResponse(String response) {
+        this.responseStr = response;
+    }
+
+}
